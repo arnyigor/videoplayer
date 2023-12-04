61982ddd4fc0e0354bc6aeb44d0a8a63e06e9917
sedoyiyu <sedoyiyu@psbank.ru>
2023-12-04 07:58:02 +0300

    add tracer

--- app/src/main/java/com/arny/mobilecinema/VideoApp.kt
+++ app/src/main/java/com/arny/mobilecinema/VideoApp.kt
@@ -2,9 +2,42 @@ package com.arny.mobilecinema
 
 import com.arny.mobilecinema.di.DaggerAppComponent
 import dagger.android.DaggerApplication
+import ru.ok.tracer.CoreTracerConfiguration
+import ru.ok.tracer.HasTracerConfiguration
+import ru.ok.tracer.TracerConfiguration
+import ru.ok.tracer.crash.report.CrashFreeConfiguration
+import ru.ok.tracer.crash.report.CrashReportConfiguration
+import ru.ok.tracer.disk.usage.DiskUsageConfiguration
+import ru.ok.tracer.heap.dumps.HeapDumpConfiguration
 import timber.log.Timber
 
-class VideoApp : DaggerApplication() {
+class VideoApp : DaggerApplication(), HasTracerConfiguration {
+    override val tracerConfiguration: List<TracerConfiguration>
+        get() = listOf(
+            CoreTracerConfiguration.build {
+                setDebugUpload(BuildConfig.DEBUG)
+            },
+            CrashReportConfiguration.build {
+                // опции сборщика крэшей
+                setEnabled(true)
+                setNativeEnabled(true)
+                setSendAnr(true)
+            },
+            CrashFreeConfiguration.build {
+                // опции подсчета crash free
+                setEnabled(true) // default false
+            },
+            HeapDumpConfiguration.build {
+                // опции сборщика хипдампов при ООМ
+                setEnabled(true)
+            },
+            DiskUsageConfiguration.build {
+                // опции анализатора дискового пространства
+                setEnabled(true)
+                setInterestingSize(3L * 1024 * 1024 * 1024) // 3GB. Default 10GB
+                setProbability(100) // ( 1 / 100 ) * 100% = 1%
+            },
+        )
     private val applicationInjector = DaggerAppComponent.builder()
         .application(this)
         .build()
