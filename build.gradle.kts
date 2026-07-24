cdc1c0e5071073cc5b84e84ba2b73c93037f2b18
Arnyigor <arnyigor@gmail.com>
2026-07-24 17:25:30 +0500

    Revert "Migrate Gradle config to Kotlin DSL and version catalog"
    This reverts commit ae7f3c76c29da155fac5119342fcab0b59a54697.

--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.parcelize) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.navigation.safeargs) apply false
-    alias(libs.plugins.secrets) apply false
-}
