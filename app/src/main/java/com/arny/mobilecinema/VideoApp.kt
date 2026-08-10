ebfc54d0b86353dd14b3f82b4a254c9d4fd99eac
Arnyigor <arnyigor@gmail.com>
2026-08-10 11:32:42 +0500

    chore: remove BuildFingerprint logging
    Drop the fingerprint log that printed the build-time BASE_LINK secret to
    logcat. Also removes now-unused ApplicationInfo/Build/Log imports.

--- app/src/main/java/com/arny/mobilecinema/VideoApp.kt
+++ app/src/main/java/com/arny/mobilecinema/VideoApp.kt
@@ -1,9 +1,6 @@
 package com.arny.mobilecinema
 
 import android.app.Application
-import android.content.pm.ApplicationInfo
-import android.os.Build
-import android.util.Log
 import com.arny.mobilecinema.di.dataModule
 import com.arny.mobilecinema.di.domainModule
 import com.arny.mobilecinema.di.presentationModule
@@ -31,29 +28,5 @@ class VideoApp : Application() {
             Timber.plant(Timber.DebugTree())
         }
 
-        logBuildFingerprint()
-    }
-
-    private fun logBuildFingerprint() {
-        val packageInfo = packageManager.getPackageInfo(packageName, 0)
-        val versionCode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            packageInfo.longVersionCode
-        } else {
-            @Suppress("DEPRECATION")
-            packageInfo.versionCode.toLong()
-        }
-        val debuggable = (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) != 0
-        Log.i(
-            "BuildFingerprint",
-            "appId=${BuildConfig.APPLICATION_ID} " +
-                "package=$packageName " +
-                "version=${packageInfo.versionName}($versionCode) " +
-                "debug=${BuildConfig.DEBUG} " +
-                "debuggable=$debuggable " +
-                "baseLink=${BuildConfig.BASE_LINK} " +
-                "sourceDir=${applicationInfo.sourceDir} " +
-                "firstInstallTime=${packageInfo.firstInstallTime} " +
-                "lastUpdateTime=${packageInfo.lastUpdateTime}"
-        )
     }
 }
